c95069754154936be739f715c1b21d3dd99b9b9d
damilola1ade <damiiyi@gmail.com>
2022-11-04 12:19:43 +0100

    Fixed time stamp on crypto details

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,34 +1,47 @@
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-import Chart from 'chart.js/auto';
-import { Col, Row, Typography } from 'antd';
+import React from "react";
+// Chart is required by chart.js, don't remove even if not used.
+// eslint-disable-next-line
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+import { Col, Row, Typography } from "antd";
 
+// Typography
 const { Title } = Typography;
 
+// Line Chart
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinPrice = [];
   const coinTimestamp = [];
 
+  // Coin Price
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinPrice.push(coinHistory?.data?.history[i].price);
   }
 
+  // Coin Timestamp
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
+    coinTimestamp.push(
+      new Date(
+        coinHistory?.data?.history[i].timestamp * 1000
+      ).toLocaleDateString()
+    );
   }
+
+  // Data
   const data = {
     labels: coinTimestamp,
     datasets: [
       {
-        label: 'Price In USD',
+        label: "Price In USD",
         data: coinPrice,
         fill: false,
-        backgroundColor: '#0071bd',
-        borderColor: '#0071bd',
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
       },
     ],
   };
 
+  // Options
   const options = {
     scales: {
       yAxes: [
@@ -43,13 +56,21 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
 
   return (
     <>
+      {/* Coin Info */}
       <Row className="chart-header">
-        <Title level={2} className="chart-title">{coinName} Price Chart </Title>
+        <Title level={2} className="chart-title">
+          {coinName} Price Chart{" "}
+        </Title>
         <Col className="price-container">
-          <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
-          <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
+          <Title level={5} className="price-change">
+            Change: {coinHistory?.data?.change}%
+          </Title>
+          <Title level={5} className="current-price">
+            Current {coinName} Price: ${currentPrice}
+          </Title>
         </Col>
       </Row>
+      {/* Chart */}
       <Line data={data} options={options} />
     </>
   );
